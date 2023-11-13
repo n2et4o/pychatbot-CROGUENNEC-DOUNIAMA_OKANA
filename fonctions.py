342235a074fa202a264954baf843431ec0542a39
n2et4o <117654110+n2et4o@users.noreply.github.com>
2023-11-13 17:23:24 +0100

    New menu

--- fonctions.py
+++ fonctions.py
@@ -16,7 +16,10 @@ def print_list(files_names):
     # Associations de chaques prénoms à une présisents par dictionnaire
 
     cles = ['Jacques1','Jacques2','Valéry','François_Mitterand1','François_Mitterand2','Emmanuel','François','Nicolas']
-    dictionnaire = {cles[i]: files_names[i] for i in range(len(cles))}
+    #dictionnaire = {cles[i]: files_names[i] for i in range(len(cles))}
+    dictionnaire ={}
+    for i in range(1,len(cles)):
+        dictionnaire[cles[i]] = files_names[i]
     print("================ dictionnaire =========================",'\n')
     for cle, valeur in dictionnaire.items():
         print(f"{cle}: {valeur}", end=' \n',)
@@ -211,7 +214,7 @@ def menu_french():
         print("Avant d'aller plus loin, vous allez devoir saisir un chemin d'accès pour faciliter mon utilisation. ")
         reponse = input("Savez vous comment ajouter un chemin d'accès ? \n"
                         "réponse : ")
-        while reponse.lower() not in ['oui', 'yes', 'bien sûr', 'je sais', 'bien sur'] and reponse.lower() not in ['non','no','je ne connais pas','je ne sais pas']:
+        while reponse.lower() not in ['oui', 'yes', 'bien sûr', 'je sais', 'bien sur'] or reponse.lower() not in ['non','no','je ne connais pas','je ne sais pas']:
             if reponse.lower() in ['oui', 'yes', 'bien sûr', 'je sais']:
                 break
             elif reponse.lower() in ['non', 'no', 'je ne connais pas', 'je ne sais pas', 'explique', 'dit moi tous']:
@@ -257,7 +260,7 @@ def menu_english():
         print("Before going any further, you'll need to enter an access path to facilitate my use. ")
         reponse = input("Do you know how to add a path ? \n"
                         "answer : ")
-        while reponse.lower() not in ['oui', 'yes', 'of course', 'i know','sure'] and reponse.lower() not in ['non', 'no', "I don't know", "I don't know", 'Explain', 'Tell me all about it']:
+        while reponse.lower() not in ['oui', 'yes', 'of course', 'i know','sure'] or reponse.lower() not in ['non', 'no', "I don't know", "I don't know", 'Explain', 'Tell me all about it']:
             if reponse.lower() in ['oui', 'yes', 'of course', 'i know','sure']:
                 break
             elif reponse.lower() in ['non', 'no', "I don't know", "I don't know", 'Explain', 'Tell me all about it']:
@@ -302,7 +305,7 @@ def menu_spanish():
         print("Antes de seguir adelante, tendrás que introducir una ruta para facilitar su uso. ")
         reponse = input("¿Sabes cómo añadir una ruta ? \n"
                         "responder : ")
-        while reponse.lower() not in ['oui', "sí",'si', "por supuesto", "lo sé"] and reponse.lower() not in ['non', 'no', 'no lo sé', 'no lo sé', 'explícame', 'cuéntamelo todo']:
+        while reponse.lower() not in ['oui', "sí",'si', "por supuesto", "lo sé"] or reponse.lower() not in ['non', 'no', 'no lo sé', 'no lo sé', 'explícame', 'cuéntamelo todo']:
             if reponse.lower() in ['oui', "sí",'si', "por supuesto", "lo sé"]:
                 break
             elif reponse.lower() in ['non', 'no', 'no lo sé', 'no lo sé', 'explícame', 'cuéntamelo todo']:
@@ -360,4 +363,81 @@ def langue():
                 time.sleep(1.5)
 
 
+
+
+def menu_french_new():
+    run = True
+    while run == True:
+        print("Bonjour !\n")
+        time.sleep(1)
+        print("Je suis ChatBot \n")
+        time.sleep(1)
+        print("Tapez l'un des chiffres \n")
+        time.sleep(1)
+        print("1 - Commençez\n2 - Options\n3 - Comment ça marche ? \n4 - À quoi sert ?\n5 - Quittez\n")
+        lancement = input(":")
+        while lancement not in ['1', '2', '3', '4', '5']:
+            lancement = input(":")
+        if lancement == '1':
+            print("Avant d'aller plus loin, vous allez devoir saisir un chemin d'accès pour faciliter mon utilisation. ")
+            reponse = input("Savez vous comment ajouter un chemin d'accès ?\n1 - Oui\n2 - Non\n3 - Retour\n4 _ Quittez\n:")
+            while reponse not in ['1', '2', '3', '4']:
+                reponse = input(":")
+            if reponse == '1':
+                pass
+            elif reponse == '2':
+                print("Pour ajouter un chemin d'accès, vous devez aller sur le fichier ou dossier que vous voulez son chemin d'accès, puis faites un clique droit. \nSéléctionnez 'Copy Path' puis 'Absolue path' et il ne vous restera plus qu'à coller le chemin d'accès que vous venez de copier \n")
+                pass
+            elif reponse == '3':
+                continue
+            elif reponse == '4':
+                break
+            directory = None
+            while directory is None:
+                directory = input(("Saissisez votre chemin d'accès du dossier 'speeches' : "))
+        elif lancement == '2':
+            option = input("Tapez un chiffre\n1 - Langue\n2 - Crédit\n3 - Retour\n:")
+            while option not in ['1', '2', '3']:
+                option = input(":")
+            if option == '1':
+                langue = input("1 - Français\n2 - English\n3 - Espanõl\n4 - Retour\n: ")
+                while langue not in ['1', '2', '3', '4']:
+                    langue = input(":")
+                if langue == '1':
+                    continue
+                elif langue == '2':
+                    print("Bientôt disponible.\n ")
+                    continue
+                elif langue == "3":
+                    print("Bientôt disponible. \n")
+                    continue
+                elif langue == '4':
+                    continue
+            elif option == '2':
+                print("Crédit du programme")
+                time.sleep(1)
+                print("réalisé par : \n")
+                time.sleep(1)
+                print("Joss DOUNIAMA OKANA\n")
+                time.sleep(1)
+                print("Liam CROGUENNEC\n")
+                time.sleep(1)
+                print("Projet Python\n")
+                time.sleep(1)
+                print("Groupe BN 2023-2024\n")
+                time.sleep(1)
+                print("Fin\n")
+                time.sleep(1)
+                continue
+            elif option == '3':
+                continue
+        elif lancement == '3':
+            print("\nCommençez puis suivez les instructions. \n")
+            continue
+        elif lancement == '4':
+            print("\nBientôt disponible. \n")
+            continue
+        elif lancement == '5':
+            run = False
+    return directory
 
